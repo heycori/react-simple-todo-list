9716606d1f28208bf622ae13b47c3368c036cd08
lxieyang <lxieyang@umich.edu>
2019-11-19 20:08:13 -0500

    update readme

---
+++ src/secret.firebase.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBE3Vj48kqRbWAWabaOe3wF8-fTV6sXleA",
+  authDomain: "ssui-react-simple-todo.firebaseapp.com",
+  databaseURL: "https://ssui-react-simple-todo.firebaseio.com",
+  projectId: "ssui-react-simple-todo",
+  storageBucket: "ssui-react-simple-todo.appspot.com",
+  messagingSenderId: "246494016167",
+  appId: "1:246494016167:web:ab1531982b53be9ec7a182"
+};
